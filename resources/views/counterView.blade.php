2d72a96b53c7b20c77f24223a462f395e56fe047
Prakhar Srivastava <prakhar@bluelupin.com>
2024-10-10 18:07:38 +0530

    Updated

---
+++ resources/views/counterView.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Counter Application in Livewire</title>
+
+    @livewireStyles
+</head>
+<body>
+
+    <livewire:counter/>
+    @livewireScripts
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Livewire New Project</title>
+    @livewireStyles
+</head>
+<body>
+    
+    <livewire:counter/>
+    @livewireScripts
+</body>
+</html>
